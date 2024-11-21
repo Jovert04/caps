447e1d91e62bff40b200740c21f1a3fed17118d9
daisuke120302 <iamejaymendoza@gmail.com>
2024-11-21 14:45:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,35 +5,10 @@ const PORT = 3000;
 // Middleware to parse JSON data
 app.use(express.json());
 
-// Sample data for parking slots and camera IPs
-let parkingSlots = {
-    1: false,  // Free
-    2: true,   // Occupied
-    3: false,
-    4: true,
-    5: false,
-    6: false,
-    7: true,
-    8: false,
-    9: true,
-    10: false,
-    11: false,
-    12: false,
-    13: true,
-    14: false,
-    15: true,
-    16: false,
-    17: false,
-    18: false
-};
-
 const cameraIPs = [
     "http://192.168.1.21:",  // Channel 1
     "http://camera-ip-address-2",  // Channel 2
     "http://camera-ip-address-3",  // Channel 3
-    "http://camera-ip-address-4",  // Channel 4
-    "http://camera-ip-address-5",  // Channel 5
-    "http://192.168.1.21:",    // Channel 6
 ];
 
 // Endpoint to get parking slot statuses
